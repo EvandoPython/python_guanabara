ea8eff9a17e0db940098f6d7ac6d39f1a6a45582
Evando Silva <evando_5@hotmail.com>
2023-08-12 10:03:10 -0300

    at

---
+++ Guanabara python/desafio03.py
@@ -0,0 +1,5 @@
+print("=====Desafio 3 ========")
+valor1 = int(input("Digite o primeiro: "))
+valor2 = int(input("Digite o segundo: "))
+soma = valor1 + valor2
+print(f'A soma de {valor1} + {valor2} é {soma}')
