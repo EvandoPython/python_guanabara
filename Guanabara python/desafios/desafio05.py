b660ab3080509b209900ca5ea929470605784a23
Evando Silva <evando_5@hotmail.com>
2023-08-19 15:54:22 -0300

    att

---
+++ Guanabara python/desafios/desafio05.py
@@ -0,0 +1,4 @@
+n = int(input("Entre com um numero inteiro: "))
+s = n+1
+a = n-1
+print(f'O Sucessor é: {s} o antecessor é {a}')
---
+++ Guanabara python/desafios/desafio06.py
@@ -0,0 +1,5 @@
+n = int(input("Entre com um numero inteiro: "))
+d = n * 2
+t = n * 3
+r = n **(1/2)
+print(f'O seu dobro é: {d} o triplo é: {t} a raiz é {r}')
