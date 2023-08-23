ff62bd1f927d3e2b167a43cc7426ce912c7eb3cf
Evando Silva <evando_5@hotmail.com>
2023-08-22 21:41:09 -0300

    att

--- Guanabara python/desafios/desafio06.py
+++ Guanabara python/desafios/desafio06.py
@@ -2,4 +2,4 @@ n = int(input("Entre com um numero inteiro: "))
 d = n * 2
 t = n * 3
 r = n **(1/2)
-print(f'O seu dobro é: {d} o triplo é: {t} a raiz é {r}')
+print(f'O seu dobro é: {d} o triplo é: {t} a raiz é {r:.2f}')
