fb8cd021532e458a6f50bfbdc44c486f71c03f7b
Evando Silva <evando_5@hotmail.com>
2023-08-28 21:50:27 -0300

    att

---
+++ Guanabara python/desafios/desafio10.py
@@ -0,0 +1,5 @@
+n = float(input("Quanto dinheiro você tem na carteira R$: "))
+dolar = n / 4.87
+print(f'Com {n} você pode comprar US$: {dolar:.2f}')
+
+    
---
+++ Guanabara python/desafios/desafio11.py
@@ -0,0 +1,8 @@
+l = float(input("Largura da Parede: "))
+a = float(input("Altura da Parede: "))
+area = l * a
+tinta = area / 2
+print(f'A sua Parede tem a dimensão de {l} x {a} e sua area é de {area}  ')
+print(f'Para pintar essa parede voce precisara de {tinta}l')
+
+    
