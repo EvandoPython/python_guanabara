69b4ca5daa3ea23a902df6dde222474c6cf69256
Evando Silva <evando_5@hotmail.com>
2023-09-02 14:56:18 -0300

    att

---
+++ Guanabara python/desafios/desafio13.py
@@ -0,0 +1,3 @@
+salario = float(input("Qual é o salario do funcionario R$: "))
+aumento = salario +( salario * 15 / 100)
+print(f'O funcionario que ganhava {salario} com 15% de aumento passa a receber {aumento:.2f}')
